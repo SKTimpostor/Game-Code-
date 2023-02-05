dd21d1e7be5707d3178484cf978e7cef9dbfd5d6
Thiago Luiz <programmeralves@gmail.com>
2023-02-05 12:24:42 -0300

    commit
    adicionando area interna

--- script/script.js
+++ script/script.js
@@ -8,3 +8,15 @@ function verificar(){
         alert('SENHA INCORRETA');
     }
 }
+
+
+function verificarInterno(){
+    const x = document.querySelector('#inputsenha');
+    const i = x.value;
+    if(i == 3851 ){
+        document.getElementById('section').style.display = 'flex';
+        document.getElementById('verifica-senha').style.display = 'none';
+    }else{
+        alert('SENHA INCORRETA');
+    }
+}
